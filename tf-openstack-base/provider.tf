e7b1a82b93b32936094045ad9ab135ae49e653ea
WillyRL <willyrobertus@gmail.com>
2024-10-19 22:56:08 +0700

    Update the openstack terraform provider version due to deprecation on 3.0.0

--- tf-openstack-base/provider.tf
+++ tf-openstack-base/provider.tf
@@ -2,9 +2,9 @@ terraform {
   required_providers {
     openstack = {
       source = "terraform-provider-openstack/openstack"
+      version = "~> 2.1.0"
     }
   }
-  required_version = ">= 0.13"
 }
 
 provider "openstack" {}
